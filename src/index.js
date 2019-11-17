f830c27f649257d12b76fd0925b0fbdfaf685d36
Grant <grantmcnally.dev@gmail.com>
2019-11-17 15:23:00 +0000

    wrapped App component with a Provider component with store being passed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import App from './components/app/app';
-
+import reducers from './reducers';
 require('./styles/base.scss');
 
 if (module.hot) {
@@ -10,6 +12,8 @@ if (module.hot) {
 }
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('app'),
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
 );
