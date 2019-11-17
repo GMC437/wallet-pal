da93cf987f94cd647215d0adecfcdb6dc6b1f23d
Grant <grantmcnally.dev@gmail.com>
2019-11-17 16:24:27 +0000

    basic markup with no styling done for Wallet component

--- src/components/wallet/index.js
+++ src/components/wallet/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { balance, currency, name } from '../../data';
+
+const currentBalance =
+  balance < 0 ? `-${currency}${Math.abs(balance)}` : `${currency}${balance}`;
 
 const Wallet = () => {
-    return <div>Wallet</div>;
-}
+  return (
+    <div>
+      Wallet
+      <div>
+        <div>{name}</div>
+        <div>{currentBalance}</div>
+        <div>Your balance is negative</div>
+        <input type="number" aria-label="balance amount" />
+        <div>
+          <button type="button">Increase</button>
+          <button type="button">Decrease</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Wallet;
