a28ea661a71a534b0bda6cf33a96be11c634b2df
Grant <grantmcnally.dev@gmail.com>
2019-11-17 19:38:40 +0000

    BalanceHistory displays list from redux state

--- src/components/history-balance/index.js
+++ src/components/history-balance/index.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const BalanceHistory = () => {
-    return <div>Balance History</div>
-}
+const getBalanceHistoryList = balanceHistory => {
+    return balanceHistory.map((item, index) => {
+      return (
+        <li key={index}>
+          ITEM: {index}
+          <div>Balance: £{item.balance}</div>
+          <div>Amount: £{item.amount}</div>
+          <br />
+        </li>
+      );
+    });
+  };
 
-export default BalanceHistory;
+const BalanceHistory = ({ balanceHistory }) => {
+  const BalanceHistoryList = getBalanceHistoryList(balanceHistory);
+  return (
+    <div>
+      <div>Balance History</div>
+      <ul>
+        {BalanceHistoryList}
+      </ul>
+    </div>
+  );
+};
+
+const mapStateToProps = state => {
+  console.log(state);
+  return { balanceHistory: state.balanceHistory };
+};
+
+export default connect(mapStateToProps)(BalanceHistory);
