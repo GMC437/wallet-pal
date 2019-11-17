8fb7e6844c8cca7ac99e5e7324a776d415b1160f
Grant <grantmcnally.dev@gmail.com>
2019-11-17 17:28:50 +0000

    updated Wallet to use input for actions on buttons

--- src/components/wallet/index.js
+++ src/components/wallet/index.js
@@ -1,35 +1,65 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { balance, currency, name } from '../../data';
+import { currency, name } from '../../data';
 import { decreaseBalance, increaseBalance } from '../../actions';
 import { getBalance } from '../../reducers';
 
-
 const Wallet = ({ currentBalance, decreaseBalance, increaseBalance }) => {
-    const balanceItem = currentBalance < 0 ? `-${currency}${Math.abs(currentBalance)}` : `${currency}${currentBalance}`;
-  
-    return (
+  const roundedBalance = Math.round(currentBalance * 100) / 100;
+  const balanceItem =
+    currentBalance < 0
+      ? `-${currency}${Math.abs(roundedBalance)}`
+      : `${currency}${roundedBalance}`;
+  let input;
+
+  return (
     <div>
       Wallet
       <div>
         <div>{name}</div>
         <div>{balanceItem}</div>
-        {currentBalance < 0 ?<div>Your balance is negative</div>: <br/>}
-        <input type="number" aria-label="balance amount" />
+        {currentBalance < 0 ? <div>Your balance is negative</div> : <br />}
+        <input
+          type="number"
+          aria-label="balance amount"
+          ref={node => (input = node)}
+        />
         <div>
-          <button type="button" onClick={() => increaseBalance(5, currentBalance)} >Increase</button>
-          <button type="button" onClick={() => decreaseBalance(5, currentBalance)} >Decrease</button>
+          <button
+            type="button"
+            onClick={() => {
+              if (!input.value) {
+                return;
+              }
+              increaseBalance(parseFloat(input.value), currentBalance);
+            }}
+          >
+            Increase
+          </button>
+          <button
+            type="button"
+            onClick={() => {
+              if (!input.value) {
+                return;
+              }
+              decreaseBalance(parseFloat(input.value), currentBalance);
+            }}
+          >
+            Decrease
+          </button>
         </div>
       </div>
     </div>
   );
 };
 
-const mapStateToProps = (state) => {
-    console.log(state)
-    return {
-        currentBalance: getBalance(state.balanceHistory)
-    }
-}
+const mapStateToProps = state => {
+  console.log(state);
+  return {
+    currentBalance: getBalance(state.balanceHistory),
+  };
+};
 
-export default connect(mapStateToProps, { decreaseBalance, increaseBalance })(Wallet);
+export default connect(mapStateToProps, { decreaseBalance, increaseBalance })(
+  Wallet
+);
