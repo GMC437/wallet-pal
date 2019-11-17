9ff12476362b6adaa3f17058769c19cb31da0e50
Grant <grantmcnally.dev@gmail.com>
2019-11-17 15:12:21 +0000

    added tests for balanceHistoryReducer

--- src/reducers/index.test.js
+++ src/reducers/index.test.js
@@ -1,9 +1,41 @@
-import { initState } from './index';
+import { initState, balanceHistoryReducer } from './index';
+
+const mockData = {
+  id: 1,
+  amount: 1,
+  balance: 12,
+};
 
 describe('reducers', () => {
-  describe('balanceHistory', () => {
+  const { balanceHistory } = initState;
+
+  describe('balanceHistoryReducer', () => {
     it('should provide the initial state', () => {
-      expect(initState.balanceHistory.length).toBe(1);
+      expect(balanceHistory.length).toBe(1);
+    });
+
+    it('should handle INCREASE_BALANCE action', () => {
+      const data = { ...mockData, type: 'INCREASE_BALANCE' };
+      const balanceArr = balanceHistoryReducer(balanceHistory, data);
+      
+      expect(balanceArr.length).toBe(2);
+      expect(balanceArr[1].balance).toBe(13);
+    });
+
+    it('should handle DECREASE_BALANCE action', () => {
+      const data = { ...mockData, type: 'DECREASE_BALANCE' };
+      const balanceArr = balanceHistoryReducer(balanceHistory, data);
+
+      expect(balanceArr.length).toBe(2);
+      expect(balanceArr[1].balance).toBe(11);
+    });
+
+    it('should ignore unknown actions', () => {
+      const data = { ...mockData, type: 'fake' };
+      const balanceArr = balanceHistoryReducer(balanceHistory, data);
+      
+      expect(balanceArr.length).toBe(1);
+      expect(balanceArr[0].balance).toBe(12);
     });
   });
 });
