a6d218ed91b338f04922a4a9e77791fdca67280e
Grant <grantmcnally.dev@gmail.com>
2019-11-17 17:42:37 +0000

    updated App component by importing BalanceHistory component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import styles from './app.scss';
 import Wallet from '../wallet';
+import HistoryBalance from '../history-balance'; 
 
 class App extends React.Component {
   render() {
     return (
       <div className={styles.wrapper}>
         <Wallet />
+        <HistoryBalance />
       </div>
     );
   }
