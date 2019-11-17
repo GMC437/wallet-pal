9b35b9bd6d839148a6f85044dc04a296f3846b9d
Grant <grantmcnally.dev@gmail.com>
2019-11-17 15:13:10 +0000

    added balanceHistoryReducer to index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,3 +3,31 @@ import data from '../data';
 export const initState = {
   balanceHistory: [{ balance: data.balance, amount: 0, id: 0 }],
 };
+
+export const balanceHistoryReducer = (
+  state = initState.balanceHistory,
+  action
+) => {
+  switch (action.type) {
+    case 'INCREASE_BALANCE':
+      return [
+        ...state,
+        {
+          id: action.id,
+          amount: action.amount,
+          balance: action.balance + action.amount,
+        },
+      ];
+    case 'DECREASE_BALANCE':
+      return [
+        ...state,
+        {
+          id: action.id,
+          amount: action.amount,
+          balance: action.balance - action.amount,
+        },
+      ];
+    default:
+      return state;
+  }
+};
