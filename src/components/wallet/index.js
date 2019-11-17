d6c48730e65eeb756db5dc69621e1efd09c6ed9f
Grant <grantmcnally.dev@gmail.com>
2019-11-17 17:07:58 +0000

    connected Wallet to redux state

--- src/components/wallet/index.js
+++ src/components/wallet/index.js
@@ -1,25 +1,35 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { balance, currency, name } from '../../data';
+import { decreaseBalance, increaseBalance } from '../../actions';
+import { getBalance } from '../../reducers';
 
-const currentBalance =
-  balance < 0 ? `-${currency}${Math.abs(balance)}` : `${currency}${balance}`;
 
-const Wallet = () => {
-  return (
+const Wallet = ({ currentBalance, decreaseBalance, increaseBalance }) => {
+    const balanceItem = currentBalance < 0 ? `-${currency}${Math.abs(currentBalance)}` : `${currency}${currentBalance}`;
+  
+    return (
     <div>
       Wallet
       <div>
         <div>{name}</div>
-        <div>{currentBalance}</div>
-        <div>Your balance is negative</div>
+        <div>{balanceItem}</div>
+        {currentBalance < 0 ?<div>Your balance is negative</div>: <br/>}
         <input type="number" aria-label="balance amount" />
         <div>
-          <button type="button">Increase</button>
-          <button type="button">Decrease</button>
+          <button type="button" onClick={() => increaseBalance(5, currentBalance)} >Increase</button>
+          <button type="button" onClick={() => decreaseBalance(5, currentBalance)} >Decrease</button>
         </div>
       </div>
     </div>
   );
 };
 
-export default Wallet;
+const mapStateToProps = (state) => {
+    console.log(state)
+    return {
+        currentBalance: getBalance(state.balanceHistory)
+    }
+}
+
+export default connect(mapStateToProps, { decreaseBalance, increaseBalance })(Wallet);
