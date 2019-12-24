f457620f7c168f33fa78b631c8d95d608ad40763
Grant <grantmcnally.dev@gmail.com>
2019-12-24 22:29:49 +0000

    updated balanceItem test with mock data

--- src/components/balance-item/index.test.js
+++ src/components/balance-item/index.test.js
@@ -2,26 +2,42 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import BalanceItem from './index';
 
+const mockData = {
+  balance: 15,
+  currency: '$',
+};
+
 it('BalanceItem renders correctly', () => {
+  const { balance, currency } = mockData;
   const wrap = shallow(
-    <BalanceItem currency="$" currentBalance={15} isWallet={false} />
+    <BalanceItem
+      currency={currency}
+      currentBalance={balance}
+      isWallet={false}
+    />
   );
 
-  expect(wrap.text()).toEqual('$15');
+  expect(wrap.text()).toEqual(`${currency}${balance}`);
 });
 
 it('BalanceItem renders correctly with currentBalance is negative number', () => {
+  const { balance, currency } = mockData;
   const wrap = shallow(
-    <BalanceItem currency="$" currentBalance={-15} isWallet={false} />
+    <BalanceItem
+      currency={currency}
+      currentBalance={-balance}
+      isWallet={false}
+    />
   );
 
-  expect(wrap.text()).toEqual('-$15');
+  expect(wrap.text()).toEqual(`-${currency}${balance}`);
 });
 
 it('BalanceItem renders correctly with isWallet is true', () => {
+  const { balance, currency } = mockData;
   const wrap = shallow(
-    <BalanceItem currency="$" currentBalance={15} isWallet={true} />
+    <BalanceItem currency={currency} currentBalance={balance} isWallet={true} />
   );
 
-  expect(wrap.text()).toEqual('$15');
+  expect(wrap.text()).toEqual(`${currency}${balance}`);
 });
